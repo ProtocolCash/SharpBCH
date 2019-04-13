d1eafdfbe995d3d4b90a6397bea0f09ec968f288
Protocol Cash <ProtocolCash@users.noreply.github.com>
2019-04-13 01:19:23 -0700

    Add Block Decoding - Add Block namespace with Block and Header classes - Add TX and Merkle to user dictionary

---
+++ SharpBCH/Block/Block.cs
@@ -0,0 +1,159 @@
+﻿/*
+ * Copyright (c) 2019 ProtocolCash
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using SharpBCH.Bitcoin;
+using SharpBCH.Transaction;
+
+namespace SharpBCH.Block
+{        
+    
+    /// <summary>
+    ///     Exception specific to Block decoding and input issues
+    ///     - thrown by the public functions of this class
+    ///     - should wrap an innerException
+    /// </summary>
+    public class BlockException : Exception
+    {
+        public BlockException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    /// <summary>
+    ///     Represents and decodes a full bitcoin block
+    /// </summary>
+    class Block : Serializer
+    {
+        public BlockHeader Header { get; private set; }
+        public Transaction.Transaction[] Transactions { get; private set; }
+
+        public uint BlockSize { get; private set; }
+        public string BlockHash => Header.BlockHashHex;
+
+        public bool LengthMatch { get; private set; }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Constructor
+        ///     - creates an empty block object
+        ///     - used for LiteDB queries
+        /// </summary>
+        public Block() : base(new List<byte>())
+        {
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Constructor
+        ///     - decodes the block data
+        /// </summary>
+        /// <param name="blockBytes">raw block as byte array</param>
+        public Block(IEnumerable<byte> blockBytes) : base(blockBytes)
+        {
+            try
+            {
+                Decode();
+            }
+            catch (Exception e)
+            {
+                throw new BlockException("Error decoding block.", e);
+            }
+        }
+
+        /// <summary>
+        ///     Decode - Decodes a raw bitcoin block
+        /// </summary>
+        private void Decode()
+        {
+            BlockSize = (uint)ByteData.Length;
+
+            // header is 80bytes
+            Header = new BlockHeader(ReadSlice(80));
+
+            // get the number of transactions
+            Transactions = new Transaction.Transaction[ReadVarLenInt()];
+
+            // read all the transactions
+            for (var i = 0; i < Transactions.Length; ++i)
+                // decode the transaction
+                Transactions[i] = DecodeTX();
+
+            // validate length - should be at the end of the block
+            LengthMatch = Offset == ByteData.Length;
+        }
+
+        /// <summary>
+        ///     Decode - Decodes a raw bitcoin transaction
+        /// </summary>
+        private Transaction.Transaction DecodeTX()
+        {
+            var origOffset = Offset;
+            // tx version - uint32
+            var txVersion = ReadUInt();
+
+            // get the number of inputs - vin length
+            var inputs = new Input[ReadVarLenInt()];
+
+            // read all the inputs
+            for (var i = 0; i < inputs.Length; ++i)
+                inputs[i] = new Input
+                {
+                    Hash = ReadSlice(32),
+                    Index = ReadUInt(),
+                    Script = ReadSlice(
+                        (int)ReadVarLenInt()), // script length maximum is 520 bytes, so casting to int should be fine
+                    Sequence = ReadUInt()
+                };
+
+
+            // get the number of outputs - v out length
+            var outputs = new Output[ReadVarLenInt()];
+
+            // read all the outputs
+            for (var i = 0; i < outputs.Length; ++i)
+                outputs[i] = new Output
+                {
+                    // script length maximum is 1650 bytes, so casting to int should be fine
+                    // https://github.com/Bitcoin-ABC/bitcoin-abc/blob/master/src/policy/policy.h#L46
+                    Value = ReadUInt64(),
+                    Script = ReadSlice(
+                        (int)ReadVarLenInt()) 
+                };
+
+            // transaction lock_time
+            var lockTime = ReadUInt();
+
+            // hash the entire transaction for the TXID
+            var txLength = Offset - origOffset;
+            Offset = origOffset;
+
+            var byteData = ReadSlice(txLength);
+
+
+            return new Transaction.Transaction(byteData, BlockHash, txVersion, inputs, outputs, lockTime);
+        }
+    }
+}
---
+++ SharpBCH/Block/Header.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * Copyright (c) 2019 ProtocolCash
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using SharpBCH.Bitcoin;
+using SharpBCH.Util;
+
+namespace SharpBCH.Block
+{
+    /// <summary>
+    ///     Represents a bitcoin block header
+    ///     https://en.bitcoin.it/wiki/Block_hashing_algorithm
+    /// </summary>
+    public class BlockHeader : Serializer
+    {
+        public uint BlockVersion { get; private set; }
+        public string PrevBlockHash { get; private set; }
+        public byte[] MerkleRootHash { get; private set; }
+        public uint TimeStamp { get; private set; }
+        public uint DiffTarget { get; private set; }
+        public uint Nonce { get; private set; }
+        public string BlockHashHex { get; private set; }
+
+        public bool LengthMatch { get; private set; }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Constructor
+        ///     - creates an empty block header object
+        ///     - used for LiteDB queries
+        /// </summary>
+        public BlockHeader() : base(new List<byte>())
+        {
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Constructor
+        ///     - decodes the block header data
+        /// </summary>
+        /// <param name="headerBytes">raw block header as byte array</param>
+        public BlockHeader(IEnumerable<byte> headerBytes) : base(headerBytes)
+        {
+            try
+            {
+                Decode();
+            }
+            catch (Exception e)
+            {
+                throw new BlockException("Error decoding block header.", e);
+            }
+        }
+
+        /// <summary>
+        ///     Decode - Decodes a raw bitcoin block header
+        /// </summary>
+        private void Decode()
+        {
+            // block version number
+            BlockVersion = ReadUInt();
+
+            // previous block hash
+            PrevBlockHash = ByteHexConverter.ByteArrayToHex(ReadSlice(32).Reverse().ToArray());
+
+            // merkle root hash
+            MerkleRootHash = ReadSlice(32);
+
+            // block timestamp (seconds since 1970-01-01T00:00 UTC)
+            TimeStamp = ReadUInt();
+
+            // difficulty target in compact format
+            DiffTarget = ReadUInt();
+
+            // nonce
+            Nonce = ReadUInt();
+
+            // strict validation - we should be at the end of the header
+            LengthMatch = Offset == ByteData.Length;
+
+            // block hash = sha256(sha256(header_data)) -> reverse byte data -> convert to hex
+            SHA256 sha256 = new SHA256Managed();
+            BlockHashHex = ByteHexConverter.ByteArrayToHex(sha256.ComputeHash(sha256.ComputeHash(ByteData)).Reverse().ToArray());
+        }
+    }
+}
